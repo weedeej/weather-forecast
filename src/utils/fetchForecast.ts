27666b9276173a7ecfc325062ae81022009d8458
weedeej <work.deeej@gmail.com>
2024-01-31 19:53:50 +0800

    Added limit in function

--- src/utils/fetchForecast.ts
+++ src/utils/fetchForecast.ts
@@ -5,15 +5,16 @@ import { getLongLat } from "./getLongLat";
 /**
  * Sends a request to http://api.openweathermap.org for forecast
  * @param cityName
+ * @param limit
  * @returns Forecast object
  */
-export async function fetchForecast(cityName: string) {
+export async function fetchForecast(cityName: string, limit: number = 5) {
   const geoData = await getLongLat(cityName);
   const {lon, lat} = geoData[0];
 
   let resp: Response | undefined;
   try {
-    resp = await fetch(`${apiBaseUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&cnt=11&appid=${weatherApiKey}`);
+    resp = await fetch(`${apiBaseUrl}/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&cnt=${limit}&appid=${weatherApiKey}`);
   } catch {
     throw "An error has occured while fetching Forecast";
   }
