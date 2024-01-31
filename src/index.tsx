b9a0db363633190d030324515b05eaac1700a0bb
weedeej <work.deeej@gmail.com>
2024-01-31 15:57:44 +0800

    Implemented Auth0Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +10,15 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+        domain="weedeej.us.auth0.com"
+        clientId="oEkhmigrLhg8Eq6lQxXkLIiPCR7UylW5"
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
