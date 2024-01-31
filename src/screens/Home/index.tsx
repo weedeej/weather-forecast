3b21f4854be5b7c8f0472c4f46a5735804c8afe8
weedeej <work.deeej@gmail.com>
2024-01-31 20:34:36 +0800

    Redesigned UI

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,9 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button, CircularProgress, Stack } from "@mui/material";
+import { Button, CircularProgress, Divider, IconButton, Stack } from "@mui/material";
 import { useState } from "react";
 import { HomeContent, Landing, Weather } from "./Content";
 import { Forecast } from "../../types";
+import { IconHome, IconLogin2, IconNews } from "@tabler/icons-react";
 
 enum PageContent {
   LANDING,
@@ -14,7 +15,7 @@ export function Home() {
   const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
   const [content, setContent] = useState<PageContent>(PageContent.LANDING);
   const [weatherForecast, setWeatherForecast] = useState<Forecast | null>(null);
-  
+
   function onHomeClick() {
     setContent(PageContent.HOME);
   }
@@ -38,27 +39,32 @@ export function Home() {
   let main = <></>;
   switch (content) {
     case PageContent.HOME:
-      main = <HomeContent setWeatherForecast={updateWeatherForecast} displayWeather={displayWeather} forecast={weatherForecast}/>
+      main = <HomeContent setWeatherForecast={updateWeatherForecast} displayWeather={displayWeather} forecast={weatherForecast} />
       break;
     case PageContent.WEATHER:
-      main = <Weather forecast={weatherForecast}/>
+      main = <Weather forecast={weatherForecast} />
       break;
     default:
       main = <Landing />
       break;
   }
-  
-  if (isLoading) return <CircularProgress/>;
+
+  if (isLoading) return <CircularProgress />;
   return (
     <Stack gap={2}>
-      {main}
-      <Stack direction="row" gap={2}>
-        {!isAuthenticated && <Button variant="outlined" onClick={onLoginClick}>Login</Button>}
-        {(isAuthenticated && content !== PageContent.LANDING) && <Button variant="outlined" onClick={onBackClick}>Landing</Button>}
-        {(isAuthenticated && content !== PageContent.HOME) && (
-            <Button variant="contained" onClick={onHomeClick}>{content === PageContent.WEATHER ? "BACK" : "HOME"}</Button>
-          )}
+      <Stack direction="row" justifyContent="space-between" gap={2}>
+        {
+          isAuthenticated && (
+            <Stack direction="row" width="100%" gap={0} alignItems="center" justifyContent="center">
+              <Button variant={content == PageContent.LANDING ? "contained" : "text"} sx={{ borderRadius: 0 }} startIcon={<IconNews />} onClick={onBackClick}>LANDING</Button>
+              <Button variant={content == PageContent.HOME ? "contained" : "text"} sx={{ borderRadius: 0 }} startIcon={<IconHome />} onClick={onHomeClick}>DASHBOARD</Button>
+            </Stack>
+          )
+        }
+        {!isAuthenticated && <Button variant="contained" startIcon={<IconLogin2 />} onClick={onLoginClick}>Login</Button>}
       </Stack>
+      <Divider />
+      {main}
     </Stack>
   );
 }
