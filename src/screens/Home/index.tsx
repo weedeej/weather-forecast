057be3cf488c9ca32a6c80f52a1f27c9725198b6
weedeej <work.deeej@gmail.com>
2024-01-31 16:52:57 +0800

    Added weather state

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Box, Button, CircularProgress, Stack, Typography } from "@mui/material";
 import { useState } from "react";
 import { HomeContent, Landing, Weather } from "./Content";
+import { Forecast } from "../../types";
 
 enum PageContent {
   LANDING,
@@ -12,26 +13,10 @@ enum PageContent {
 export function Home() {
   const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
   const [content, setContent] = useState<PageContent>(PageContent.LANDING);
-
-  let main = <></>;
-  switch (content) {
-    case PageContent.HOME:
-      main = <HomeContent/>
-      break;
-    case PageContent.WEATHER:
-      main = <Weather/>
-      break;
-    default:
-      main = <Landing />
-      break;
-  }
-
+  const [weatherForecast, setWeatherForecast] = useState<Forecast | null>(null);
+  
   function onHomeClick() {
     setContent(PageContent.HOME);
-  }
-
-  function onWeatherClick() {
-    setContent(PageContent.WEATHER);
   }
 
   function onBackClick() {
@@ -40,6 +25,23 @@ export function Home() {
 
   function onLoginClick() {
     loginWithRedirect();
+  }
+
+  function updateWeatherForecast(forecast: Forecast | null) {
+    setWeatherForecast(forecast);
+  }
+
+  let main = <></>;
+  switch (content) {
+    case PageContent.HOME:
+      main = <HomeContent setWeatherForecast={updateWeatherForecast}/>
+      break;
+    case PageContent.WEATHER:
+      main = <Weather forecast={weatherForecast}/>
+      break;
+    default:
+      main = <Landing />
+      break;
   }
   
   if (isLoading) return <CircularProgress/>;
@@ -51,7 +53,6 @@ export function Home() {
         {(isAuthenticated && content !== PageContent.LANDING) && <Button variant="outlined" onClick={onBackClick}>Landing</Button>}
         <Stack direction="row" gap={2}>
           {(isAuthenticated && content !== PageContent.HOME) && <Button variant="contained" onClick={onHomeClick}>Home</Button>}
-          {(isAuthenticated && content !== PageContent.WEATHER) && <Button variant="contained" onClick={onWeatherClick}>Weather</Button>}
         </Stack>
       </Stack>
     </Stack>
