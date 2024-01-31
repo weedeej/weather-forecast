27ca2ec31431ee70b80245f8ddb9aaf080ade443
weedeej <work.deeej@gmail.com>
2024-01-31 18:18:02 +0800

    Added another landing page variant

--- src/screens/Home/Content/Landing.tsx
+++ src/screens/Home/Content/Landing.tsx
@@ -1,9 +1,15 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Typography } from "@mui/material";
 
 export function Landing() {
+  const {isAuthenticated} = useAuth0();
+
+  const text = !isAuthenticated ?
+    "Welcome to the weather forecast web application. Please login with your Github user to use the application and view the weather in your city." :
+    "Welcome to the weather forecast web application. You can search cities in your Home/Dashboard."
   return (
     <Typography variant="h6">
-      Welcome to the weather forecast web application. Please login with your Github user to use the application and view the weather in your city.
+      {text}
     </Typography>
   )
 }
